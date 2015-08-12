e92c19537a935ba56509008b55999bdb26f4219b
Ao Hong <halberthong714@gmail.com>
2015-08-12 19:26:19 -0400

    Add gitignore, Add starting file of Arrays_and_Strings.java

---
+++ Arrays_and_Strings.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Arrays_and_Strings {
+	public static void main(String[] args) {
+		Arrays_and_Strings as = new Arrays_and_Strings();
+		as.run();
+	}
+
+	public void run() {
+		HelloWorld();
+	}
+
+	private void HelloWorld() {
+		System.out.println("Hello World");
+	}
+
+}
