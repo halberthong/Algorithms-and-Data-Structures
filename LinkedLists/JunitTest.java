0dcf7b274bb78554b25913a62061638838778e5c
Ao Hong <halberthong714@gmail.com>
2015-08-20 21:48:26 -0400

    Change AssertEquals comparisons in deleteDups1,2

--- LinkedLists/JunitTest.java
+++ LinkedLists/JunitTest.java
@@ -14,7 +14,7 @@ public class JunitTest {
         ll = new LinkedLists();
     }
 
-    @Ignore
+    // @Ignore
     @Test
     public void test2() {
         ListNode n1 = new ListNode(1);
@@ -24,13 +24,13 @@ public class JunitTest {
         n2.add(2);
 
         try{
-            assertEquals(ll.deleteDups1(n1).toString(), n2.toString());
+            assertEquals(ll.deleteDups1(n1), n2);
         } catch(AssertionError e) {
             System.out.println(e.getMessage());
         }
     }
 
-    @Ignore
+    // @Ignore
     @Test
     public void test3() {
         ListNode n1 = new ListNode(1);
@@ -40,25 +40,27 @@ public class JunitTest {
         n2.add(2); n2.add(3);
 
         try{
-            assertEquals(ll.deleteDups2(n1).toString(), n2.toString());
+            assertEquals(ll.deleteDups2(n1), n2);
         } catch(AssertionError e) {
             System.out.println(e.getMessage());
         }
 
     }
 
+    @Ignore
     @Test
     public void test4() {
         ListNode n1 = new ListNode(1);
         ListNode n2 = new ListNode(4);
 
         n1.add(2); n1.add(3); n1.add(4); n1.add(5); n1.add(6);
-        
+
         try{
             assertEquals(ll.nthToLast(n1, 3).equals(n2), true);
         } catch(AssertionError e) {
             System.out.println(e.getMessage());
         }
     }
+
 
 }
