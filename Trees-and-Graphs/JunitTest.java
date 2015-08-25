a93ff0ca6b9958e29f05dfbab12f1cce7f7e15a7
Ao Hong <halberthong714@gmail.com>
2015-08-24 21:04:05 -0400

    Initial commit to Trees-and-Graphs

---
+++ Trees-and-Graphs/JunitTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+import java.util.*;
+
+public class JunitTest {
+    /*
+    Add @Ignore before @Test if you don't run the test case
+    */
+    @BeforeClass
+    public static void oneTimeSetUp() {
+    }
+
+    @Test
+    public void test1() {
+        TreeNode t1 = new TreeNode(1);
+        t1.left = new TreeNode(2);
+        t1.right = new TreeNode(3);
+        TreeNode t2 = new TreeNode(1);
+        t2.left = new TreeNode(2);
+        t2.right = new TreeNode(3);
+        try{
+            assertEquals(t1, t2);
+        } catch(AssertionError e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ Trees-and-Graphs/TreeNode.java
@@ -0,0 +1,28 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int x) {
+        this.val = x;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof TreeNode) {
+            TreeNode t = (TreeNode) obj;
+            if(t.val == this.val) {
+                boolean res = false;
+                if(t.left != null && this.left != null) {
+                    res = t.left.equals(this.left);
+                }
+                if(res && t.right != null && this.right != null) {
+                    res = t.right.equals(this.right);
+                }
+                if(t.left == null && t.right == null && this.left == null && this.right == null){
+                    res = true;
+                }
+                return res;
+            }
+        }
+        return false;
+    }
+}
