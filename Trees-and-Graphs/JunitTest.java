02324ecdba0f0ca766702df5f57059c3a3a45c81
Ao Hong <halberthong714@gmail.com>
2015-09-11 13:28:11 -0400

    Add iterative preorder traversal and cross check with recursive one

--- Trees-and-Graphs/JunitTest.java
+++ Trees-and-Graphs/JunitTest.java
@@ -17,13 +17,13 @@ public class JunitTest {
     @Ignore
     @Test
     public void test1() {
-        TreeNode t1 = new TreeNode(1);
-        t1.left = new TreeNode(2);
-        t1.right = new TreeNode(3);
-        TreeNode t2 = new TreeNode(1);
-        t2.left = new TreeNode(2);
-        t2.right = new TreeNode(3);
-        t1.display();
+        TreeNode t1 = new TreeNode(5);
+        t1.left = new TreeNode(1);
+        t1.right = new TreeNode(10);
+        TreeNode t2 = new TreeNode(5);
+        t2.add(1);
+        t2.add(10);
+        tg.preOrderTraversal(t1, 1);
         try{
             assertEquals(t1, t2);
         } catch(AssertionError e) {
@@ -31,11 +31,15 @@ public class JunitTest {
         }
     }
 
+    // @Ignore
     @Test
     public void test2() {
         TreeNode t1 = new TreeNode(3);
         t1.add(1); t1.add(2); t1.add(4); t1.add(5);
-        t1.display();
-        tg.preOrderTraversal(t1, 1);
+        try{
+            assertEquals(tg.preOrderTraversal(t1, 1), tg.preOrderTraversal(t1, 2));
+        } catch(AssertionError e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- Trees-and-Graphs/TreesAndGraphs.java
+++ Trees-and-Graphs/TreesAndGraphs.java
@@ -1,16 +1,42 @@
 import java.util.*;
 
 public class TreesAndGraphs {
-    public void preOrderTraversal(TreeNode root, int method) {
+    public List<Integer> preOrderTraversal(TreeNode root, int method) {
         /*
         method: 1 for recursive, 2 for iterarive
         */
+        List<Integer> res = new LinkedList<Integer>();
         if (method == 1) {
-            if (root != null) {
-                System.out.print(" " + root.val);
-                preOrderTraversal(root.left, 1);
-                preOrderTraversal(root.right, 1);
+            preOrderHelper(root, res);
+            return res;
+        } else if (method == 2) {
+            if (root == null) {
+                return res;
             }
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.push(root);
+            while (!stack.isEmpty()) {
+                TreeNode tmp = stack.pop();
+                if (tmp.right != null) {
+                    stack.push(tmp.right);
+                }
+                if (tmp.left != null) {
+                    stack.push(tmp.left);
+                }
+                res.add(tmp.val);
+            }
+            return res;
+        } else {
+            return res;
         }
+    }
+
+    public void preOrderHelper(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        res.add(root.val);
+        preOrderHelper(root.left, res);
+        preOrderHelper(root.right, res);
     }
 }
